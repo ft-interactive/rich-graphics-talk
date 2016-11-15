0354a4d5b3286cc2ede1967af68c001de7dece82
Martin Stabe <martinstabe@gmail.com>
2016-11-15 18:28:10 +0000

    Update config file

--- config/article.js
+++ config/article.js
@@ -1,29 +1,29 @@
 export default () => ({ // eslint-disable-line
 
   // link file UUID
-  id: '',
+  id: 'b4f57138-0ca7-11e2-b175-00144feabdc0',
 
   // canonical URL of the published page
   // https://ig.ft.com/sites/rich-graphics-talk get filled in by the ./configure script
-  url: 'https://ig.ft.com/sites/rich-graphics-talk',
+  url: 'https://ig.ft.com/sites/straight-of-hormuz-corridor-of-power',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  publishedDate: new Date('2012-10-04T17:11:22Z'),
 
-  headline: 'Energy: Corridor of power',
+  headline: 'Straight of Hormuz: the corridor of power',
 
   // summary === standfirst (Summary is what the content API calls it)
   summary: 'Fears are rising that Iran’s cornered leadership could block the world’s most critical oil choke point.',
 
   topic: {
     name: 'Oil',
-    url: '/foo',
+    url: 'https://www.ft.com/topics/themes/Oil',
   },
 
   relatedArticle: {
-    text: 'Related article »',
-    url: 'https://en.wikipedia.org/wiki/Politics_and_the_English_Language',
+    text: '',
+    url: '',
   },
 
   mainImage: {
@@ -37,15 +37,15 @@ export default () => ({ // eslint-disable-line
   // Byline can by a plain string, markdown, or array of authors
   // if array of authors, url is optional
   byline: [
-    { name: 'Javier Blas', url: '/foo/bar' },
-    { name: 'Chris Campbell' },
+    { name: 'Javier Blas', url: 'https://www.ft.com/stream/authorsId/Q0ItMDAwMDY4NQ==-QXV0aG9ycw==' },
+    { name: 'Chris Campbell', url: 'https://www.ft.com/stream/authorsId/YjEwZjcxNDktMGNkYy00MzE4LThjYjgtNTY4NDFmNmY2MWRj-QXV0aG9ycw==' },
   ],
 
   // Appears in the HTML <title>
-  title: '',
+  title: 'Straight of Hormuz: the corridor of power',
 
   // meta data
-  description: '',
+  description: 'Fears are rising that Iran’s cornered leadership could block the world’s most critical oil choke point.',
 
   /*
   TODO: Select Twitter card type -
