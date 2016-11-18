2e6230cdf8459a0b5dc754d3bca0c11571acffbc
Martin Stabe <martinstabe@gmail.com>
2016-11-18 16:13:56 +0000

    onward journey change

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default () => ({
   relatedContent: [
-    { rows: 1, list: 'list/graphics' },
+    { rows: 1, list: 'thing/ZmFmYTUxOTItMGZjZC00YmJkLWJlZTQtMmY3ZDZiOWZkYmYw-VG9waWNz' },
     { rows: 1, list: 'list/highlights' },
   ],
 });
