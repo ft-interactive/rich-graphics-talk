56076027a6c12a5de0843c688a04de8e816593bf
Martin Stabe <martinstabe@gmail.com>
2016-11-18 16:19:05 +0000

    Onward journey stream updated

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default () => ({
   relatedContent: [
     { rows: 1, list: 'thing/ZmFmYTUxOTItMGZjZC00YmJkLWJlZTQtMmY3ZDZiOWZkYmYw-VG9waWNz' },
-    { rows: 1, list: 'list/highlights' },
+    { rows: 1, list: 'thing/TnN0ZWluX0dMX0lS-R0w=' },
   ],
 });
